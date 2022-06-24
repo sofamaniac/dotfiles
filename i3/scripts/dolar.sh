a698549768bdbdd55aa796f2e73f7021eb7d1200
Antoine Grimod <antoine.grimod@gmail.com>
2022-06-24 18:34:31 +0200

    clean up i3 config

--- i3/scripts/dolar.sh
+++
@@ -1,7 +0,0 @@
-# Script creado por Exo del grupo de Telegram de ArchlinuxLatinoamerica
-
-[dolar]                                          
-label=DL                                         
-interval=60                                      
-separator=true                                   
-command=curl -s https://banco.santanderrio.com.ar/exec/cotizacion/index.jsp | grep -aoP "(?<=<td>)[^<]*" | sed -n 3p
