b3cdc7bb180fa42480eb6efb39b8d1076e86bc4d
Antoine Grimod <antoine.grimod@gmail.com>
2023-07-12 11:39:03 +0200

    using hostname to avoid multiple version of files

--- run_at_start.sh
+++ run_at_start.sh
@@ -1,11 +1,22 @@
+# screen setup
+HOSTNAME=$(hostname)
+FILE=~/dotfiles/screenlayout/$HOSTNAME.sh
+if test -f "$FILE"; then
+  $FILE
+fi
 playerctld daemon
 xkbcomp -w 0 ~/dotfiles/keymaps/azerty $DISPLAY
+picom -b --config ~/.config/picom/picom.conf
 ~/.config/i3/bg_setter.sh
 xsettingsd &
 ~/.config/polybar/launch_polybar.sh &
-picom -b --config ~/.config/picom/picom.conf
-nextcloud &
 nm-applet &
 dunst -config ~/.config/dunst/dunstrc &
 kdeconnect-indicator &
 unclutter &
+
+if [ "$name" = "astolfo" ]; then
+  nextcloud &
+fi
+
+
