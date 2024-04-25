f48e97720753a33ef43e6ea9d6f62ab9bc076490
Antoine Grimod <antoine.grimod@gmail.com>
2024-04-26 00:54:03 +0200

    polybar config fixes

--- config/polybar/launch_polybar.sh
+++ config/polybar/launch_polybar.sh
@@ -1,8 +1,8 @@
-killall polybar-msg
-killall polybar
+pkill polybar-msg
+pkill polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload principal &
   done
 else
   polybar --reload example &
