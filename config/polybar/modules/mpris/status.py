3a3839d6431dd2bab359ce8cea298841e8e5152d
Antoine Grimod <antoine.grimod@gmail.com>
2024-07-09 11:28:33 +0200

    better player status

--- config/polybar/modules/mpris/status.py
+++ config/polybar/modules/mpris/status.py
@@ -11,58 +11,79 @@ def get_track():
 
     proc = subprocess.Popen(["playerctl", "-p", player, "-s", "metadata", "title"], stdout=subprocess.PIPE)
     title, err = proc.communicate()
-    return title.decode().strip()
+    new_title = title.decode().strip()
+    if new_title == get_track.old_title:
+        return new_title, False
+    else:
+        get_track.old_title = new_title
+        return new_title, True
+
+get_track.old_title = ""
 
 def get_length(s):
-    proc = subprocess.Popen(["echo", s], stdout=subprocess.PIPE)
     length_proc = subprocess.Popen(["wc", "-L"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     out, err = length_proc.communicate(input=s.encode())
     return int(out.decode().strip())
 
 def make_string(title, start):
-    end = 0
-    while end < len(title) and get_length(title[start:end]) < WIDTH:
-        end += 1
-    if get_length(title[start:end])> WIDTH:
+    end = max(len(title), WIDTH)
+    while get_length(title[start:end])> WIDTH:
         end -= 1
     final_size = get_length(title[start:end])
     final_string = title[start:end] + (' '*(WIDTH - final_size))
     return final_string
 
-def prep_title(old_title):
-    title = get_track()
-    title = title + ' ' * WIDTH + title
-    if old_title == title:
-        return old_title, False
-    return title, True
+def make_all(title):
+    true_length = len(title)
+    if get_length(title) < WIDTH:
+        return [title]
+    title = prep_title(title)
+    res = []
+    for i in range(true_length + WIDTH//2):
+        res.append(make_string(title, i))
+    return res
+
+
+def prep_title(title):
+    title = title + ' ' * (WIDTH//2) + title
+    return title
 
 
 SONG_REFRESH=1.500
-IN_BETWEEN_PAUSE=1.500
+IN_BETWEEN_PAUSE=5.000
 ADVANCE_DELAY = 0.400
 
 def main():
     title = prep_title("")
     start = 0
+    last_time = time.time()
     last_song_refresh=time.time()
     time_beginning = time.time()
     time_advance = time.time()
+    strings = []
     while True:
-        if time.time() - last_song_refresh:
-            title, changed = prep_title(title)
+        t = time.time()
+        if t - last_song_refresh:
+            title, changed = get_track()
             if changed:
                 start = 0
-                time_beginning = time.time()
-        if start == 0 and time.time() - time_beginning > IN_BETWEEN_PAUSE:
+                time_beginning = t
+                strings = make_all(title)
+        if start == 0 and t - time_beginning > IN_BETWEEN_PAUSE:
             start += 1
-            time_advance = time.time()
-        if start > 0 and time.time() - time_advance > ADVANCE_DELAY:
+            time_advance = t
+        if start > 0 and t - time_advance > ADVANCE_DELAY:
             start += 1
-            time_advance = time.time()
+            time_advance = t
 
-        start = start % len(title)
-        print(make_string(title, start))
+        if start == len(strings):
+            time_beginning = t
+
+        start = start % len(strings)
+        print(strings[start])
         time.sleep(0.100)
+        last_time = t
+
 
 
 main()
