bf5e75c9bedf050a2dc17d25481a4f493b3a3d04
Antoine Grimod <antoine.grimod@gmail.com>
2025-02-04 17:53:32 +0100

    iter over all folders

--- setup_syncthing.sh
+++ setup_syncthing.sh
@@ -22,11 +22,12 @@ Help()
 CheckTailscale() {
   # check if tailscale is up
   echo "Checking tailscale connection"
-  STATUS="$(tailscale status | grep $TARGET | grep active)"
+  STATUS="$(tailscale ping $TARGET | grep pong)"
   if [[ -z "${STATUS// /}" ]]; then
     echo "Not connected to tailscale" >&2
     exit 1
   fi
+  echo "Connected to tailscale"
 }
 
 GetTargetID() {
@@ -39,6 +40,28 @@ GetTargetID() {
   fi
 }
 
+ListAll() {
+    FOLDERS=$(ssh $TARGET syncthing cli config folders list)
+    for folder in $FOLDERS; do
+      echo $folder
+    done
+}
+
+# Setup the connection to the provided folder's ID
+ConnectOne() {
+  local id=
+  id="$1"
+  local path=
+  local label=
+  
+  path=$(ssh cli config folders $1 path get)
+  label=$(ssh cli config folders $1 label get)
+
+  echo "Settig up $label"
+
+  syncthing cli config folders add --id="$id" --path="$path" --label="$label"
+}
+
 Connect() {
   # check if already connected to target
   CON="$(syncthing cli show connections)"
@@ -49,14 +72,17 @@ Connect() {
     echo "Already connected to $TARGET"
     exit 0
   else 
+
     echo "Connecting to $TARGET"
     # Adding devices on both sides
-    syncthing cli config devices add --device-id=$ID --name=$TARGET --auto-accept-folders
+    syncthing cli config devices add --device-id=$ID --name=$TARGET
     ssh $TARGET syncthing cli config devices add --device-id=$(syncthing --device-id) --name=$(hostname)
     echo "Setting up folders"
-    # Adding shared folders
-    syncthing cli config folders add --id=x9yls-aaswg --path=~/Nextcloud
-    syncthing cli config folders add --id=1v6b3-54wrm --path=~/Musics
+    local folders=
+    folders=$(ssh $TARGET syncthing cli config folders list)
+    for folder in $folders; do
+      ConnectOne $folder
+    done
   fi
 }
 
@@ -71,6 +97,7 @@ eval set -- "$PARSED"
 
 TARGET=
 TARGET_NAME=
+IS_LIST=false
 while true; do
   case "$1" in
     -h|--help)
@@ -79,6 +106,9 @@ while true; do
     -n|--name)
       TARGET_NAME=$2
       shift 2;;
+    -l | --list)
+      IS_LIST=true
+      shift;;
     --)
       shift
       break;;
@@ -99,7 +129,13 @@ if [[ -z $TARGET_NAME ]]; then
   TARGET_NAME=$TARGET
 fi
 
+if $IS_LIST; then
+  ListAll
+  exit 0
+fi
+
 CheckTailscale
 GetTargetID
 Connect
 
+echo "Check at http://127.0.0.1:8384/# that everything is ok"
